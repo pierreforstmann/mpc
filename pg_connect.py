533a8e4e0394b552289f3b15cc9967bd514adbf4
Pierre <pierre.forstmann@free.fr>
2023-07-01 14:05:12 +0200

    pg_connect.py: use pg_connect.ini with configparser

--- pg_connect.py
+++ pg_connect.py
@@ -4,18 +4,27 @@
 # PG connection pool
 #
 # Copyright Pierre Forstmann 2023
-#------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+import configparser
 import psycopg2
 from psycopg2 import pool
+
+config = configparser.ConfigParser()
+config.read("pg_connect.ini")
+pg_user = config['default']['user']
+pg_password = config['default']['password']
+pg_port = config['default']['port']
+pg_host = config['default']['host']
+pg_database = config['default']['database']
 
 connection_pool = psycopg2.pool.SimpleConnectionPool(
                      1,     # minimum number of connections
                      1,     # maximum number of connections
-                     user="test",
-                     password="test",
-                     host="localhost",
-                     port="5442",
-                     database="postgres"
+                     user=pg_user,
+                     password=pg_password,
+                     host=pg_host,
+                     port=pg_port,
+                     database=pg_database
                    )
 
 # Get a connection from the pool
