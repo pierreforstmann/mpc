f439f202a8f82625ed18a8a5d98c2d0a2175ebb7
Pierre <pierre.forstmann@free.fr>
2023-12-21 10:17:11 +0100

    analyze.py: added bugrefd dictionary to store bugno references

--- analyze.py
+++ analyze.py
@@ -8,20 +8,26 @@ import sys
 import mailbox
 import re
 
+bugrefd = {}
+
 def get_mail_subjects(filename):
     mbox = mailbox.mbox(filename)
     regex = re.compile('[\s\S][#]\d+')
     for message in mbox:
        subject = message['subject']
-       print("subject => ", subject, end='')
+       # print("subject => ", subject, end='')
        if regex.search(subject):
-           print(" bugno => ", regex.search(subject).group())
-       else:
-           print(" bugno => None", '\n')
-
+           bugref = regex.search(subject).group()
+           # print(" bugno => ", bugref)
+           if bugref not in bugrefd:
+                bugrefd[bugref] = 1
+           else:
+               bugrefd[bugref] += 1
+               
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python analyze.py mbox")
         sys.exit(1)
 
     get_mail_subjects(sys.argv[1])
+    print(bugrefd)
