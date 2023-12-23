63614292747dcfb9eb7f3f79c64e3bf1d22e7af3
Pierre <pierre.forstmann@free.fr>
2023-12-23 15:27:22 +0100

    analyze.py: month_number string representation must have leading 0 if less than 10

--- analyze.py
+++ analyze.py
@@ -11,23 +11,31 @@ import re
 bugrefd = {}
 
 def get_mail_subjects(filename):
+    global bugrefd
     mbox = mailbox.mbox(filename)
     regex = re.compile('[\s\S][#]\d+')
     for message in mbox:
        subject = message['subject']
-       # print("subject => ", subject, end='')
-       if regex.search(subject):
-           bugref = regex.search(subject).group()
-           # print(" bugno => ", bugref)
-           if bugref not in bugrefd:
-                bugrefd[bugref] = 1
-           else:
-               bugrefd[bugref] += 1
-               
+       if subject is not None:
+           if regex.search(subject):
+                bugref = regex.search(subject).group()
+                # print(" bugno => ", bugref)
+                if bugref not in bugrefd:
+                    bugrefd[bugref] = 1
+                else:
+                    bugrefd[bugref] += 1
+    print("current bugrefd => ", bugrefd)
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python analyze.py mbox")
+        print("Usage: python analyze.py year")
         sys.exit(1)
-
-    get_mail_subjects(sys.argv[1])
-    print(bugrefd)
+    for month_number in range(1,13):
+        if month_number <= 9:
+            month_string = "0" + str(month_number)
+        else:
+            month_string = str(month_number)
+        mbox = "pgsql-bugs." + sys.argv[1] + month_string
+        print("mbox =>", mbox)
+        get_mail_subjects(mbox)
+    print("final bugrefd =>", bugrefd)
